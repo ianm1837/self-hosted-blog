e5804c6c0ec0de6ed57c5861d074094dd980a490
ianm1837 <ian@iwm.dev>
2023-02-07 17:17:56 -0800

    Created main files; added home template; started server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const app = express();
+
+// Path: routes/index.js
+// Compare this snippet from routes/index.js:
+// const express = require('express');
+// const home = require('./home');
+// const router = express.Router();
+//
