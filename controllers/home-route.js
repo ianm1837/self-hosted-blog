be22fa80491913e4718e43157dd7c4a3aa91071e
ianm1837 <ian@iwm.dev>
2023-02-13 21:16:49 -0800

    Render all posts to homepage

--- controllers/home-route.js
+++ controllers/home-route.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
+const { Post } = require('../models');
 
-router.get('/', (req, res) => {
-  res.render('home');
+router.get('/', async (req, res) => {
+  try {
+    const dbDashboardData = await Post.findAll();
+
+    let posts = dbDashboardData.map((posts) => ({
+      title: posts.title,
+      content: posts.content,
+      timestamp: posts.timestamp,
+      user_id: posts.user_id,
+    }));
+
+    console.log('list of user posts: ' + JSON.stringify(posts));
+    res.render('home', {
+      posts,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
