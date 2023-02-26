28f92ee603609c4a45039a632b8ff9c06f87c60b
ianm1837 <ian@iwm.dev>
2023-02-25 19:57:35 -0800

    fix ordering of page redirection

--- controllers/index.js
+++ controllers/index.js
@@ -5,11 +5,11 @@ const userRoutes = require('./user');
 
 // you are here: /
 
+router.use('/posts', postsRoutes);
+router.use('/user', userRoutes);
+
 router.get('/', (req, res) => {
   res.redirect('/posts');
 });
-
-router.use('/posts', postsRoutes);
-router.use('/user', userRoutes);
 
 module.exports = router;
